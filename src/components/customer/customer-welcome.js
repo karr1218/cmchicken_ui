fdbed50a95f142977960beb78769f83dcdbc9262
karr1218 <karr1218@gmail.com>
2022-05-26 01:29:41 -0500

    creating app with using npx create-react-app . & npm install react-router-dom basic folders setup

---
+++ src/components/customer/customer-welcome.js
@@ -0,0 +1,8 @@
+export default function CustomerWelcome() {
+
+    return (
+        <>
+            <h1>Welcome to the CM Chicken!</h1>
+        </>
+    )
+}
---
+++ src/components/order/order-welcome.js
@@ -0,0 +1,9 @@
+export default function OrderWelcome() {
+
+    return (
+        <>
+            <h1>Welcome to the Order Page!</h1>
+        </>
+    )
+
+}
